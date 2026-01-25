ca87cddd0bb0cf01344c561dfd29a315699ca933
zh4icy <nadumazhanleymae@gmail.com>
2026-01-25 23:00:57 +0800

    Initial commit - add IT121 files

---
+++ Assignment-1-scores/main.py
@@ -0,0 +1,22 @@
+score1 = 100
+score2 = 70
+score3 = 75
+
+
+print("Score 1:", score1)
+if score1 >= 75:
+    print ("Passed!")
+else:
+    print("Failed!")
+print()
+print("Score 2:", score2)
+if score2 >= 75:
+    print ("Passed!")
+else:
+    print("Failed!")
+print()
+print("Score 3:", score3)
+if score3 >= 75:
+    print ("Passed!")
+else:
+    print("Failed!")
---
+++ Lab-activity1-variable/var.py
@@ -0,0 +1,17 @@
+name = "Zhanley Mae N. Labajo"
+age = 18
+height = 1.62
+has_work = False
+
+
+print("Fullname:", name)
+print(type(name))
+print()
+print("Age:", age)
+print(type(age))
+print()
+print("Height:", height)
+print(type(height))
+print()
+print("Currently Working?:", has_work)
+print(type(has_work))
